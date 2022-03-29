f24a126295474e3305e65befe6902628a44aa578
Váradi Niki <74589652+varadin23@users.noreply.github.com>
2022-03-29 12:33:22 +0200

    Add files via upload

---
+++ sodokuGUI/sodokuGUI/sodokuGUI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sodokuGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            StreamReader sr = new StreamReader("feladvanyok.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream) ;
+            
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+    }
+}
